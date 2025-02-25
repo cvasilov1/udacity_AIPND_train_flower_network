b68d326f9f77a8587c8416cadd489a2a7ee843a4
cvasilov1 <cameliavasilov@gmail.com>
2025-02-25 06:24:37 +0100

    Initial commit: Project setup without tracking dataset

---
+++ untitled0_leftover_code.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 24 16:48:51 2025
+
+@author: camel
+"""
+
+import json
+
+with open('cat_to_name.json', 'r') as f:
+    cat_to_name = json.load(f)
+    
+cd /c/Users/camel/Desktop/Projects/MY_COURSE/second_project/aipnd-project
